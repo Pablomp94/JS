d59f9ee759fd86bff7fda45e3be9de37481a1ad4
Pablomp94 <1744623@alu.murciaeduca.es>
2023-10-24 14:27:01 +0200

    javaScript

--- Ejs/altaPersonas/main.js
+++ Ejs/altaPersonas/main.js
@@ -1,21 +1,40 @@
-var arr;
-
-function Persona(name, age, dni){
-    
-    this.nombre = name,
-
-    this.edad = age,
-
-    this.id = dni;
-}
-
-const pablo = new Persona("Pablo", 18, "123456789A");
-
-const david = new Persona("David",19,"9776127389D");
-
+var arr,opc = 5;
 arr = [];
+
+const pablo = new Persona("Pablo", "Morales", "2004");
+
+const david = new Persona("David","asdasd","2003");
 
 arr.push(pablo);
 arr.push(david);
 
-console.log(pablo);
+
+opc = prompt("Intrduce la opción a realizar: 1.Crear 2.Eliminar 3.Actualizar 4.Ver");
+
+if(opc == 1){
+    crear();
+}
+
+
+
+function Persona(name, sname, nac){
+    
+    this.nombre = name,
+
+    this.apellido = sname,
+
+    this.fecha = nac;
+
+    crear();
+}
+
+function crear(){
+    var princ = prompt("Introduce el titulo del contacto");
+    var name = prompt("Introduce el nombre de la persona a añadir");
+    var sname = prompt("Introduce el apellido de la persona a añadir");
+    var nac = prompt("Introduce la fecha de nacimiento de la persona a añadir");
+
+    princ = new Persona(name,sname,nac);
+
+    arr.push(princ);
+}
