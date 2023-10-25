622084973b20dfc781b310001ae7b474c86735fb
Pablomp94 <1744623@alu.murciaeduca.es>
2023-10-25 10:21:59 +0200

    javaScript

--- Ejs/altaPersonas/main.js
+++ Ejs/altaPersonas/main.js
@@ -1,43 +1,84 @@
-var arr = [],opc;
+var arr = [],
+  opc;
 
+const pablo = new Persona("Pablo Morales", "18", "04/11/2004");
 
-const pablo = new Persona("Pablo", "Morales", "2004");
-
-const david = new Persona("David", "asdasd", "2003");
+const david = new Persona("David asdasd", "19", "10/01/2004");
 
 arr.push(pablo);
 arr.push(david);
 
 do {
   opc = prompt(
-    "Intrduce la opción a realizar: 1.Crear 2.Eliminar 3.Actualizar 4.Ver"
+    "Intrduce la opción a realizar: 1.Crear 2.Eliminar 3.Actualizar 4.Ordenar 5.Ver"
   );
 
-  if(opc == 1) {
+  if (opc == 1) {
     crearPersona();
+  }
+
+  if (opc == 2) {
+    eliminarPersona();
   }
 
 
   if(opc == 4){
-    console.log(arr);
+    ordenar();
+  }
+
+  if (opc == 5) {
+    verPersonas();
   }
 } while (opc != 0);
 
-function Persona(name, sname, nac) {
-  (this.name = name), (this.sname = sname), (this.nac = nac);
+function Persona(name, edad, nac) {
+  (this.name = name),
+  (this.edad = edad),
+  (this.nac = nac);
 }
 
 function crearPersona() {
-    
-    var name = prompt("Introduce el nombre de la persona a añadir");
-    var sname = prompt("Introduce el apellido de la persona a añadir");
-    var nac = prompt("Introduce la fecha de nacimiento de la persona a añadir");
+  var name = prompt("Introduce el nombre y apellido de la persona a añadir");
+  var edad = prompt("Introduce la edad de la persona a añadir");
+  var nac = prompt("Introduce la fecha de nacimiento de la persona a añadir");
 
-    var princ = new Persona(name, sname, nac);
+  var princ = new Persona(name,edad, nac);
 
-    arr.push(princ);
+  arr.push(princ);
 }
 
-function eliminarPersona(){
-   //Hacer bucle for each del array, ir mostrandolo para elejir posición del array para eliminar contacto//
+function eliminarPersona() {
+  //Hacer un bucle que muestre los datos del array//
+  for(let i = 0; i < arr.length; i++){
+    document.write(i," ")
+    document.write(arr[i].name," ");
+    document.write(arr[i].edad," ");
+    document.write(arr[i].nac,"<br>"); 
+  }
+  //Preguntar por la persona a eliminar y eliminar con el delete arr[]//
+}
+
+function ordenar(){
+  let f = prompt("Ordenarlo por: 1.Ascendente 2.Descendente 3.Invertir array");
+
+  if(f == 1){
+    arr.sort(function(a, b){return a - b});
+  }
+
+  if(f == 2){
+    arr.sort(function(a, b){return b - a});
+  }
+
+  if(f == 3){
+    arr.reverse();
+  }
+}
+
+function verPersonas(){
+  for(let i = 0; i < arr.length; i++){
+    document.write(i," ")
+    document.write(arr[i].name," ");
+    document.write(arr[i].edad," ");
+    document.write(arr[i].nac,"<br>"); 
+  }
 }
