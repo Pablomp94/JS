4a1f5b7ce97665ff029a97c63d425e190a518c36
Pablomp94 <1744623@alu.murciaeduca.es>
2023-11-27 19:07:51 +0100

    javaScript

--- JUEGOV2/main.js
+++ JUEGOV2/main.js
@@ -1,26 +1,20 @@
 
 
-function juego(){
-    
-   
+function juego(){  
     let nombre = prompt("Introduce tu nombre:");
     let cantene = prompt("Introduce la cantidad de enemigos");
     temporizador();
     enemigos(cantene);
     nave();    
-
+    /*Realizo un setInterval que lo que hace es que el metodo que hace que se mueva 
+    el enemigo en las coordenadas indicadas y
+    se realize cada 20ms, haciendo que cada ese tiempo se mueva*/
+    setInterval(movimientoEnemigo, 2000);
+    
 }
 
-function enemigos(cantene){
-    for(i = 0; i < cantene; i++){
-        var imagen = document.createElement("img"); 
-        imagen.setAttribute("src", "marciano.png");
-        imagen.setAttribute("id", "marciano"); 
-        var main = document.querySelector("body"); 
-        main.appendChild(imagen);    
-    }
-}
 
+//Creo en tiempo de ejecucion la nave//
 function nave(){
     var imagen = document.createElement("img"); 
     imagen.setAttribute("src", "nave.png");
@@ -29,17 +23,24 @@ function nave(){
     main.appendChild(imagen); 
 }
 
-let teclas = new Set();
 
+//Este es el movimiento de la nave, la cual está formada por un evento que recoje la pulsacion de teclas//
+let teclas = new Set();
+//Creo el evento//
 document.addEventListener("keydown", (event) => 
     teclas.add(event.key));
 document.addEventListener("keyup",evento => teclas.delete(evento.key))
-
+//LLamo a la funcion del movimiento de la nave y compruebe cada 20ms si se esta pulsando una tecla//
 setInterval(movimientoNave, 20);
 var x, y;
 x=650;
 y=500;
+/*Funcion del movimiento de la nave, recoje las teclas wasd y con una comprobacion "if" miro que tecla se ha pulsado,
+tambien recoje el espacio para disparar*/
 function movimientoNave(){
+
+        /*Recoje el pulsacion del espacio y crea en tiempo de ejecucion la imagen del proyectil
+        en la posicion superior centrar de la nave*/ 
 
         if(teclas.has(" ")) {
 
@@ -55,18 +56,21 @@ function movimientoNave(){
             document.getElementById("disparo").style.left=xdis + "px";
             
 
+            /*Esta funcion coje el misil y le resta a la posicion vertical, lo que hace que suba un poco*/
 
             function movimientoMisil(){
                 document.getElementById("disparo").style.top=(ydis = ydis-5)+ "px";
             }
+            /*Realizo un setInterval que lo que hace es que el metodo anterior que hace que suba un poco el misil
+            se realize cada 20ms, haciendo que cada ese tiempo se suba hasta llegar arriba*/
             setInterval(movimientoMisil, 20);
         }
 
+        /*Aqui es donde recojo la tecla pulsada y si coincide le suma o le resta a la posicion x o y de la nave*/
 
         if(teclas.has("a")) {
             x = x-15;
             document.getElementById("nave").style.left=x + "px";
-            document.getElementById("nave").style.transform;
         }
         if(teclas.has("d")) {
             x = x+15;
@@ -83,6 +87,62 @@ function movimientoNave(){
     }
 
 
+
+/*Creo en tiempo de ejecucion la cantidad de enemigos que he pedido antes*/
+function enemigos(cantene){
+    for(i = 0; i < cantene; i++){
+        var imagen = document.createElement("img"); 
+        imagen.setAttribute("src", "marciano.png");
+        imagen.setAttribute("id", "marciano"); 
+        var main = document.querySelector("body"); 
+        main.appendChild(imagen);    
+    }
+}
+
+
+//Esta es la funcion que hace que se muevan de forma aleatoria los enemigos.
+//(provisional)Los enemigos aparecen de fuera de la pantalla en un punto y se van moviendo aleatoriamente hacia dentro de la pantalla.
+//(otra opcion)Aparecen arriba, dentro de la pantalla, y se mueven aleatoriamente
+//Si tocan el borde de la pantalla que no se sobrepase
+var posx = 1, posy = 1;
+function movimientoEnemigo(){
+    var uno, dos,ex, ey;
+    uno = Math.floor(Math.random()* 2);
+    dos = Math.floor(Math.random()* 2);
+
+    console.log(uno);
+    console.log(dos);
+    if(uno == 0){
+        ex = -10;
+    }
+    if(uno == 1){
+        ex = 10;
+    }
+    if(dos == 0){
+        ey = -10;
+    }
+    if(dos == 1){
+        ey = 10;
+    }
+    
+    setInterval(mov,20);
+
+    function mov(){
+        document.getElementById("marciano").style.top= (posy = (posy + ey)) + "px";
+        document.getElementById("marciano").style.left=(posx = (posx + ex)) + "px";
+    }
+
+ 
+}
+
+
+
+
+
+
+
+
+
 function detectarColisiones(laNave,naveX,naveY,enemigo,eneX,eneY){
     if( (naveX < eneX + enemigo.width) &&
         (naveX + laNave.width > eneX) &&
@@ -93,6 +153,9 @@ function detectarColisiones(laNave,naveX,naveY,enemigo,eneX,eneY){
         return false;
 }
 
+
+/*Realizo en temporizador que es un texto que dentro tiene una variable, la cual se le va sumando 1 por cada segundo, haciendo un
+reloj del tiempo que se esta jugando*/
 function temporizador(){
     let numero = 0;
     var cont = document.createElement("h1");
