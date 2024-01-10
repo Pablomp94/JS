b8ac5a7f3e4f8dfecaacfa65f4361e621b295f90
Pablomp94 <1744623@alu.murciaeduca.es>
2024-01-10 18:58:36 +0100

    javaScript

--- Ejercicio DWEC Cubos/main.js
+++ Ejercicio DWEC Cubos/main.js
@@ -28,7 +28,6 @@ function muevemeIzq(ev){
 
 
         //Para intercambiar en el DOM hay que quitar el primero y ponerlo al final de los hijos y el ultimo al primero
-        signodo = nodo.nextSibling;
         padre = nodo.parentNode;
         ultimo = nodo.parentNode.lastElementChild;
         uno = padre.removeChild(nodo);
@@ -62,6 +61,63 @@ function muevemeIzq(ev){
 }
 
 
+function muevemeDere(ev){
+    nodo = ev.target;
+    if(nodo.nodeName=="H1"){
+        nodo=nodo.parentNode;
+    }
+
+    if(nodo.nextElementSibling == null){
+        princnodo = der.firstElementChild;
+
+        let xprim = nodo.offsetLeft;
+        let yprim = nodo.offsetTop;
+        let xseg = princnodo.offsetLeft;
+        let yseg = princnodo.offsetTop;
+
+        nodo.style.left=xseg+"px";
+        nodo.style.top=yseg+"px";
+
+        princnodo.style.left=xprim+"px";
+        princnodo.style.top=yprim+"px";
+
+
+        //Para intercambiar en el DOM hay que quitar el primero y ponerlo al final de los hijos y el ultimo al primero
+        padre = nodo.parentNode;
+        primero = nodo.parentNode.firstElementChild;
+        ultimo = padre.lastElementChild;
+        dos = padre.removeChild(primero);
+
+        padre.insertBefore(uno, ultimo);
+        
+        uno = padre.removeChild(nodo);
+        
+        padre.insertBefore(dos, primero);
+    }else{
+
+        nodoSig = nodo.nextElementSibling; 
+        
+        let xprim = nodo.offsetLeft;
+        let yprim = nodo.offsetTop;
+        let xseg = nodoSig.offsetLeft;
+        let yseg = nodoSig.offsetTop;
+
+        nodo.style.left=xseg+"px";
+        nodo.style.top=yseg+"px";
+
+        nodoSig.style.left=xprim+"px";
+        nodoSig.style.top=yprim+"px";
+
+
+        //Para intercambiar en el DOM hay que quitar y ponerlo antes del hermano del nodoPrevio
+        padre = nodo.parentNode;
+        contenido = padre.removeChild(nodo.nextElementSibling);
+        padre.insertBefore(contenido, nodo);
+    }
+
+}
+
+
 /*Crear un menu al darle click derecho encima de uno de los divs de dentro del div derecho,
  dentro de este menu iran 2 opciones izquierda y derecha eligiendo asi hacia donde se movera el div*/
 
@@ -79,6 +135,7 @@ function menu(ev){
     //Hay que quitar el evento para que no se apile//
     //removeEventListener("click", muevemeIzq);
     document.getElementById("left").addEventListener("click", muevemeIzq.bind(ev.target, event));
+    document.getElementById("right").addEventListener("click", muevemeDere.bind(ev.target, event));
 } 
 })
 
